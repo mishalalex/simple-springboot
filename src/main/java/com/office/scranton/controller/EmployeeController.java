2c2ebfb679cd1e79515c63cd7eba6616d4b038a8
Mishal Alexander <mishal@Mishals-Mac-mini.local>
2025-11-04 18:41:27 +0530

    updating crud operations
    Signed-off-by: Mishal Alexander <mishal@Mishals-Mac-mini.local>

--- src/main/java/com/office/scranton/controller/EmployeeController.java
+++ src/main/java/com/office/scranton/controller/EmployeeController.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    @Autowired
-    EmployeeService employeeService;
+
+    public final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping
     public List<EmployeeDTO> getAllEmployees() {
@@ -29,12 +34,17 @@ public class EmployeeController {
     }
 
     @PutMapping(path = "/{employeeId}")
-    public String updateEmployeeById(@PathVariable Long employeeId, @RequestBody EmployeeDTO employeeDto) {
-        return "Updated Employee";
+    public boolean updateEmployeeById(@PathVariable Long employeeId, @RequestBody EmployeeDTO employeeDto) {
+        return employeeService.updateEmployee(employeeId, employeeDto);
+    }
+
+    @PatchMapping(path = "/{employeeId}")
+    public EmployeeDTO updatePartiallyEmployeeById(@PathVariable Long employeeId, @RequestBody Map<String, Object> updates) {
+        return employeeService.updateEmployeePartially(employeeId, updates);
     }
 
     @DeleteMapping(path = "/{employeeId}")
-    public String deleteEmployeeById(@PathVariable Long employeeId) {
-        return "Deleted Employee";
+    public boolean deleteEmployeeById(@PathVariable Long employeeId) {
+        return employeeService.deleteEmployee(employeeId);
     }
 }
--- src/main/java/com/office/scranton/service/EmployeeService.java
+++ src/main/java/com/office/scranton/service/EmployeeService.java
@@ -4,9 +4,12 @@ import com.office.scranton.dto.EmployeeDTO;
 import com.office.scranton.entity.EmployeeEntity;
 import com.office.scranton.repository.EmployeeRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -37,5 +40,35 @@ public class EmployeeService {
         EmployeeEntity employeeEntity = modelMapper.map(employeeDTO, EmployeeEntity.class);
         EmployeeEntity eDto = employeeRepository.save(employeeEntity);
         return modelMapper.map(eDto, EmployeeDTO.class);
+    }
+
+    public boolean updateEmployee(Long id, EmployeeDTO employeeDTO){
+        boolean exists = employeeRepository.existsById(id);
+        if(!exists) return false;
+        EmployeeEntity employeeEntity = modelMapper.map(employeeDTO, EmployeeEntity.class);
+        employeeEntity.setId(id);
+        employeeRepository.save(employeeEntity);
+        return true;
+    }
+
+    public EmployeeDTO updateEmployeePartially(Long id, Map<String, Object> fieldsTobeUpdated){
+        boolean exists = employeeRepository.existsById(id);
+        if(!exists) return null;
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        fieldsTobeUpdated.forEach((field, value) -> {
+            Field fieldTobeUpdated = ReflectionUtils.getRequiredField(EmployeeEntity.class,field);
+            fieldTobeUpdated.setAccessible(true);
+            ReflectionUtils.setField(fieldTobeUpdated,employeeEntity,value);
+            fieldTobeUpdated.setAccessible(false);
+        });
+
+        return modelMapper.map(employeeRepository.save(employeeEntity), EmployeeDTO.class);
+    }
+
+    public boolean deleteEmployee(Long id){
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).orElse(null);
+        if(employeeEntity == null) return false;
+        employeeRepository.delete(employeeEntity);
+        return true;
     }
 }
